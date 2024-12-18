a30657eb4de68b529c8e8c5a40f282792b670138
liguoliang <891776273@qq.com>
2024-12-18 15:08:53 +0800

    update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 def test():
-    print("hello world")
+    print("hello world1")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
